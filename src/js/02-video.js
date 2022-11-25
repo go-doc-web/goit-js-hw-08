aa73d01736ace56b72057785def62bc4d9588365
Oleg Gubskiy <gubskyolegod@gmail.com>
2022-11-25 04:54:32 +0200

    Делает задание 2-1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,10 +1,18 @@
+import throttle from 'lodash.throttle';
+
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 
-player.on('play', function () {
-  console.log('played the video!');
-});
+const LOCALSTORAGE_CURRENT_TIME_KEY = 'videoplayer-current-time';
 
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
+const onUpdate = e => {
+  const { seconds } = e;
+  localStorage.setItem(LOCALSTORAGE_CURRENT_TIME_KEY, seconds);
+};
+
+player.on('timeupdate', throttle(onUpdate, 1000));
+
+const lastCurrentTime = localStorage.getItem(LOCALSTORAGE_CURRENT_TIME_KEY);
+if (lastCurrentTime) {
+  player.setCurrentTime(lastCurrentTime);
+}
